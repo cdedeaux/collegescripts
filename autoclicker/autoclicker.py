51e2422b536479072a3080a60f5d0f7a3efdf038
cdedeaux <camerondedeaux@gmail.com>
2025-12-08 15:35:47 -0600

    Created a .exe for general use

--- autoclicker/autoclicker.py
+++ autoclicker/autoclicker.py
@@ -7,9 +7,8 @@ clicking = False
 def start_clicking():
     global clicking
     while clicking:
-        delay = 0.1  # seconds between clicks
         pyautogui.click() # Default is left click
-        time.sleep(0.1)  # Adjust the delay as needed
+        time.sleep(0.0001)  # Adjust the delay as needed
     
 def on_start():
     global clicking
@@ -28,16 +27,11 @@ root.title("Auto Clicker")
 mainframe = ttk.Frame(root, padding=(3, 3, 12, 12))
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 ttk.Label(mainframe, text="Cool Auto Clicker").grid(column=1, row=1, columnspan=3)
-root.bind("<Control-q>", on_stop)
-root.bind("<Command-q>", on_stop) # For MacOS
 
-root.bind("<Control-s>", on_start)
-root.bind("<Command-s>", on_start) # For MacOS
 
 ttk.Button(mainframe, text="Start", command=on_start).grid(column=1, row=3, sticky=W)
 ttk.Button(mainframe, text="Stop", command=on_stop).grid(column=2, row=3, sticky=W)
 ttk.Button(mainframe, text="Quit", command=root.quit).grid(column=3, row=3, sticky=N)
 ttk.Combobox(mainframe, values=["Left Click", "Right Click", "Middle Click"]).grid(column=1, row=5, columnspan=3, sticky=(W, E))
-#ttk.Spinbox(mainframe, from_=1, to=10).grid(column=1, row=6, columnspan=3, sticky=(W, E)) allow users to set click interval
 
 root.mainloop()
