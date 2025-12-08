fcaec78f22e4160e1b9fc777e5c0b932dc95207c
cdedeaux <camerondedeaux@gmail.com>
2025-12-08 01:12:10 -0600

    Fixed bug issues, commands dont work yet (to be fixed later)

--- autoclicker/autoclicker.py
+++ autoclicker/autoclicker.py
@@ -3,8 +3,25 @@ import time
 import threading
 from tkinter import *
 from tkinter import ttk
-
-
+clicking = False
+def start_clicking():
+    global clicking
+    while clicking:
+        delay = 0.1  # seconds between clicks
+        pyautogui.click() # Default is left click
+        time.sleep(0.1)  # Adjust the delay as needed
+    
+def on_start():
+    global clicking
+    if clicking:
+        return
+    clicking = True
+    t = threading.Thread(target=start_clicking)
+    t.daemon = True
+    t.start()
+def on_stop(event=None):
+    global clicking
+    clicking = False
 
 root = Tk()
 root.title("Auto Clicker")
@@ -12,25 +29,12 @@ root.title("Auto Clicker")
 mainframe = ttk.Frame(root, padding=(3, 3, 12, 12))
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 ttk.Label(mainframe, text="Auto Clicker").grid(column=1, row=1, columnspan=3)
+root.bind("<Control-q>", on_stop)
+root.bind("<Command-q>", on_stop) # For MacOS
 
-clicking = False
+root.bind("<Control-s>", on_start)
+root.bind("<Command-s>", on_start) # For MacOS
 
-def start_clicking():
-
-    while clicking:
-        delay = 0.1  # Delay in seconds between clicks
-        pyautogui.click() # Default is left click
-        time.sleep(delay)
-
-
-def on_start():
-    global clicking
-    clicking = True
-    threading.Thread(target=start_clicking).start()
-def on_stop():
-    global clicking
-    clicking = False
-    pass  # Implement stop functionality if needed
 ttk.Button(mainframe, text="Start", command=on_start).grid(column=1, row=3, sticky=W)
 ttk.Button(mainframe, text="Stop", command=on_stop).grid(column=2, row=3, sticky=W)
 ttk.Button(mainframe, text="Quit", command=root.quit).grid(column=3, row=3, sticky=W)
