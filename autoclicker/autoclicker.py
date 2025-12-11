46ca3596bfd044d2b45f496098c9002fdd1fb6ce
cdedeaux <camerondedeaux@gmail.com>
2025-12-10 22:47:15 -0600

    Added a command listening feature

--- autoclicker/autoclicker.py
+++ autoclicker/autoclicker.py
@@ -3,12 +3,16 @@ import time
 import threading
 from tkinter import *
 from tkinter import ttk
+from pynput import keyboard
+
+
 clicking = False
+
 def start_clicking():
     global clicking
     while clicking:
+        delay = .000001
         pyautogui.click() # Default is left click
-        time.sleep(0.0001)  # Adjust the delay as needed
     
 def on_start():
     global clicking
@@ -21,6 +25,20 @@ def on_start():
 def on_stop(event=None):
     global clicking
     clicking = False
+    
+
+def start_hotkeys():
+    with keyboard.GlobalHotKeys({
+        '<ctrl>+h': on_start,
+        '<ctrl>+i': on_stop
+    }) as h:
+        h.join()
+
+hotkey_thread = threading.Thread(target=start_hotkeys)
+hotkey_thread.daemon = True
+hotkey_thread.start()
+
+
 
 root = Tk()
 root.title("Auto Clicker")
@@ -29,9 +47,10 @@ mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 ttk.Label(mainframe, text="Cool Auto Clicker").grid(column=1, row=1, columnspan=3)
 
 
-ttk.Button(mainframe, text="Start", command=on_start).grid(column=1, row=3, sticky=W)
-ttk.Button(mainframe, text="Stop", command=on_stop).grid(column=2, row=3, sticky=W)
+ttk.Button(mainframe, text="Start (Ctrl + H)", command=on_start).grid(column=1, row=3, sticky=W)
+ttk.Button(mainframe, text="Stop (Ctrl + I)", command=on_stop).grid(column=2, row=3, sticky=W)
 ttk.Button(mainframe, text="Quit", command=root.quit).grid(column=3, row=3, sticky=N)
 ttk.Combobox(mainframe, values=["Left Click", "Right Click", "Middle Click"]).grid(column=1, row=5, columnspan=3, sticky=(W, E))
 
 root.mainloop()
+
